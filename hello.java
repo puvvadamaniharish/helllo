e2834576ba7a686a5b666b17026b263eb68f2c5e
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-27 23:36:52 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner g = new Scanner (System.in);
+		int f = g.nextInt();
+		  for (int i = 0; i<f; i++)
+		   System.out.println("Hello");
+	}
+}
